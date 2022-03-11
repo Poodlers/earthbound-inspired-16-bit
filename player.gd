de4bd48d1a40dffc00f7a18f5bb7bcd56068d304
Tomás Carmo <up202007590@g.uporto.pt>
2022-03-11 21:53:19 +0000

    Added basic attack action

--- player.gd
+++ player.gd
@@ -4,15 +4,29 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ATTACK
+}
+
 var velocity = Vector2.ZERO
+var state = MOVE
 
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
 	animationTree.active = true
+	
 
 func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			attack_state()
+		
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -21,6 +35,7 @@ func _process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -28,3 +43,13 @@ func _process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO,  FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
+func attack_state():
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+	
+func attack_state_finished():
+	state = MOVE
