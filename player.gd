c4fee278cc3feed754d78f414e5405abf18dc076
Tomás Carmo <up202007590@g.uporto.pt>
2022-03-18 01:55:27 +0000

    Added rolling feature

--- player.gd
+++ player.gd
@@ -3,14 +3,17 @@ extends KinematicBody2D
 const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
+const ROLL_SPEED = 1.5
 
 enum {
 	MOVE,
-	ATTACK
+	ATTACK,
+	ROLL
 }
 
 var velocity = Vector2.ZERO
 var state = MOVE
+var roll_vector = Vector2.DOWN
 
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
@@ -20,7 +23,7 @@ onready var swordCollison = $HitboxPivot/SwordHitbox/CollisionShape2D
 func _ready():
 	animationTree.active = true
 	swordCollison.disabled = true
-	swordHitbox.knockback_vector = Vector2.DOWN
+	swordHitbox.knockback_vector = roll_vector
 
 func _process(delta):
 	match state:
@@ -28,6 +31,8 @@ func _process(delta):
 			move_state(delta)
 		ATTACK:
 			attack_state()
+		ROLL:
+			roll_state()
 		
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -36,10 +41,12 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -50,10 +57,24 @@ func move_state(delta):
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	
 func attack_state():
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 	
 func attack_state_finished():
+	state = MOVE
+	
+func roll_state():
+	velocity = roll_vector * ROLL_SPEED * MAX_SPEED
+	animationState.travel("Roll")
+	move()
+	
+func move():
+	velocity = move_and_slide(velocity)
+	
+func roll_state_finished():
+	velocity = Vector2.ZERO
 	state = MOVE
